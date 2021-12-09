29abdd057e456d49abf16be55d4342478f056214
ignaciopineyro <ignaciopineyro@DESKTOP-T00HTRB.localdomain>
2021-12-09 13:29:52 -0300

    Up to lesson 7 completed

---
+++ my_notes/02_types.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+const firstName = 'Ignacio';
+
+const sentence1 = 'She\'s so "cool"';
+const sentence2 = `She's "cool" and I use backticks`;
+
+const badFormat = "hello my name is " + firstName + ". Nice to meet you.";
+const goodFormat = `hello my name is ${firstName}. Nice to meet you, I'm ${15+10} years old.`
