2369510450d82ae7de4996651592f2cf8be6d31a
ignaciopineyro <ignaciopineyro@DESKTOP-T00HTRB.localdomain>
2021-12-14 11:57:32 -0300

    Up to lesson 16 07.30min completed

--- my_notes/03_functions/cf.js
+++ my_notes/03_functions/cf.js
@@ -13,3 +13,59 @@ const myTotal3 = calculateBill(myOrder + herOrder);
 console.log(`Your total is $${myTotal}`);
 console.log(`Your total 2 is $${myTotal2}`);
 console.log(`Your total 3 is $${myTotal3}`);
+
+// console.log(doctorize(`Nacho`)); // Funciona porque todas las funciones declaradas con function keyword son hoisted (js las tira para arriba de todo)
+// console.log(doctorize2(`Nacho`)); // No funciona porque para js es una variable y no la hoistea
+
+function doctorize(firstName) {
+  // Regular function, con function keyword declaration
+  return `Dr. ${firstName}`;
+}
+
+// function (firstName) {  // Anonimous Function (no tiene nombre). Asi suelta tira error, pero se usa en otros contextos
+//   return `Dr. ${firstName}`;
+// }
+
+const doctorize2 = function (firstName) {
+  // Function Expression. Toma una anon funct y la asigna a una variable (el navegador intepreta que ese es el nombre de la func). Funciona bien.
+  return `Dr. ${firstName}`;
+};
+
+/* eslint-disable */
+const inchToCM = (inches) => {
+  // Fat arrow declaration. No usa la keyword function
+  return inches * 2.54;
+}
+
+const inchToCM2 = (inches) => inches * 2.54; // Arrow function. Declaracion en una linea y con return implicito
+
+const add = (a, b = 3) => a + b;
+
+const makeABaby = (first, last) => ({name: `${first} ${last}`, age:0});
+
+(function(age) { // IIFE: Immediately Invoked Function Expression. 
+  console.log('Running the Anon function');
+  return `You are cool and ${age} years old`;
+})(25);
+
+const Nacho = { // Methods. Funciones dentro de objetos
+  name: 'Nach Hho',
+  sayHi: function() { // Method
+    console.log ('Hey Nacho!');
+  },
+  shortMethod(){ // Short hand method
+    console.log(`SHORT METHOD INSIDE NACHO CLASS`)
+  },
+  arrowMethod: () => { // Arrow function
+    console.log('Arrow Method!')
+  }
+}
+
+function handleClick(){
+  console.log('Clicked!')
+}
+const button = document.querySelector('.clickMe');
+button.addEventListener('click', handleClick) // Callback function con la funct declarada antes
+button.addEventListener('click', function(){ // Le paso una anon funct como value directamente
+  console.log('Well done!')
+});
---
+++ my_notes/04_debugging/debugging.js
@@ -0,0 +1,66 @@
+const people = [
+  { name: 'Wes', cool: true, country: 'Canada' },
+  { name: 'Scott', cool: true, country: 'Merica' },
+  { name: 'Snickers', cool: false, country: 'Dog Country' },
+];
+
+people.forEach((person, index) => {
+  console.log(person.name);
+});
+
+// Console Methods
+console.log();
+console.info();
+console.error();
+console.warn();
+console.table(people); // Tira una tabla
+console.count(); // Cuenta la cantidad de veces que se ejecuto ese console.count() especificamente
+console.group(); // Agrupa todas las salidas de consola hasta groupEnd en un menu colapsable
+console.groupEnd();
+
+// Callstack
+
+// Grabbing Elements
+
+// Breakpoints
+
+// Scope
+
+// Network Requests
+
+// Break On Attribute
+
+// Some Setup Code
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function greet(name) {
+  doesntExist();
+  return `Hello ${name}`;
+}
+
+function go() {
+  const name = doctorize(greet('Wes'));
+  console.log(name);
+}
+
+const button = document.querySelector('.bigger');
+button.addEventListener('click', (e) => {
+  const newFontSize =
+    parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
+  e.currentTarget.style.fontSize = `${newFontSize}px`;
+});
+
+// A Dad joke fetch
+async function fetchDadJoke() {
+  const res = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'text/plain',
+    },
+  });
+  const joke = await res.text();
+  console.log(joke);
+  return joke;
+}
