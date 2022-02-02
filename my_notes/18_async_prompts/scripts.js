24a33e821335fb3c509b59ee94e4c87acfd7d1b8
ignaciopineyro <ignaciopineyro@DESKTOP-T00HTRB.localdomain>
2022-02-02 14:56:22 -0300

    Up to lesson 73 completed

--- my_notes/18_async_prompts/scripts.js
+++ my_notes/18_async_prompts/scripts.js
@@ -2,25 +2,52 @@ function wait(ms =0) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function ask(options) {
-    return new Promise(async function(resolve) {
-			const popup = document.createElement('form');
-			popup.classList.add('popup');
-			popup.instertAdjacentHTML('afterbegin', `
-					<fieldset>
-							<label>${options.label}</label>
-					</fieldset>
-			`);
-			
-			if(options.cancel) {
-					const skipButton = document.createElement('button');
-					skipButton.type = 'button';
-					skipButton.text = 'Cancel';
-			}
-
-			document.body.appendChild(popup);
-					
-			await wait(50);
-			popup.classList.add('open')
-    })
+async function destroyPopup(popup) {
+	popup.classList.remove('open');
+	await wait(1000);
+	popup.remove();
 }
+
+function ask(options) {
+  return new Promise(async function(resolve) {
+		const popup = document.createElement('form');
+		popup.classList.add('popup');
+		popup.insertAdjacentHTML(
+			'afterbegin', 
+			`<fieldset>
+				<label>${options.label}</label>
+				<input type="text" name="input"/>
+				<button type="submit">Submit</button>
+			</fieldset>
+		`
+		);
+			
+		if(options.cancel) {
+			const skipButton = document.createElement('button');
+			skipButton.type = 'button';
+			skipButton.text = 'Cancel';
+			popup.firstElementChild.appendChild(skipButton);
+		}
+
+		popup.addEventListener('submit', function(e) {
+			e.preventDefault();
+			resolve(e.target.input.value);
+			destroyPopup(popup);
+		}, { once: true} // Es un tercer argumento que le dice que solo escuche al evento 1 sola vez
+		);
+
+		document.body.appendChild(popup);
+				
+		await wait(50);
+		popup.classList.add('open')
+	})
+}
+
+async function askQuestion(e) {
+	const button = e.currentTarget;
+	const answer = await ask({ title: button.dataset.question, cancel: true });
+	console.log(answer);
+}
+
+const buttons = document.querySelectorAll('[data-question]');
+buttons.forEach(button => button.addEventListener('click', askQuestion));
